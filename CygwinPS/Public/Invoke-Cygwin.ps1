59a0468fb02016ee0b9429ef34d75ae3a6337b8c
domination <ps@domination.pl>
2023-05-24 03:06:09 +0200

    0.1.4

--- CygwinPS/Public/Invoke-Cygwin.ps1
+++ CygwinPS/Public/Invoke-Cygwin.ps1
@@ -52,15 +52,18 @@ function Invoke-Cygwin
     begin
     {
         $selectedEnvironment = $CygwinEnvironment;
-        if ($null -eq $CygwinEnvironment)
+        if ($null -eq $selectedEnvironment)
         {
             # get latest of already installed
             $selectedEnvironment = Get-Cygwin | Sort-Object Timestamp -Descending | Select-Object -First 1;
-            Write-Verbose ('Selecting [{0}] as current environment.' -f $selectedEnvironment.Path);
         }
+        if ($null -ne $selectedEnvironment)
+        {
+            Write-Verbose ('Selecting [{0}] as current environment.' -f $selectedEnvironment.Path);
 
-        $executable = Join-Path -Path $selectedEnvironment.Path -ChildPath 'bin/bash.exe';
-        # . "$RootDir\bin\bash.exe" --login -c $Script;
+            $executable = Join-Path -Path $selectedEnvironment.Path -ChildPath 'bin/bash.exe';
+            # . "$RootDir\bin\bash.exe" --login -c $Script;
+        }
 
         [array]$arguments = @('-c', "export PATH=/bin:/usr/bin:`$PATH`n");
 
@@ -84,40 +87,41 @@ function Invoke-Cygwin
 
     end
     {
-        $arguments[1] += ("{0}" -f ($commandsToExecute -join "`n"));
-        . $executable @arguments 2>&1
+        if ($null -ne $executable)
+        {
+            $arguments[1] += ("{0}" -f ($commandsToExecute -join "`n"));
+            . $executable @arguments 2>&1
+        }
     }
-}
+
 # SIG # Begin signature block
-# MIIFtAYJKoZIhvcNAQcCoIIFpTCCBaECAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# MIIFXgYJKoZIhvcNAQcCoIIFTzCCBUsCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUBoOheP/HQ4Qoki+5972+eWl8
-# heCgggM9MIIDOTCCAiWgAwIBAgIQ0IShyb7pW4dHi1pXwpsXLzAJBgUrDgMCHQUA
-# MCwxKjAoBgNVBAMTIVBvd2VyU2hlbGwgTG9jYWwgQ2VydGlmaWNhdGUgUm9vdDAe
-# Fw0yMDAzMTUwOTEwNTBaFw0zOTEyMzEyMzU5NTlaMBUxEzARBgNVBAMTCmRvbWlu
-# YXRpb24wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDctlfJa2xVJBNF
-# fWOhk/h++cnl/tO5May7dmrFqqMjO1/C4zcCBIarkap31lj2Ky+427sOiH5GSRV7
-# GvmjIOUYyabzDuo6N/O/Vuy4D3Dosn2JVQdvz6ZXPMfQBbtw2a0iOhsmxj143de1
-# LKCLjDfnoyK2lcEnZmCcal0fdAAGFINNMkFed+9rlS2Qm1wqxjTQGVS0czXrVf1u
-# uxGVgXFKKcW6v38GDe8Bkt/52z2XkNiNCKHkzDwVVL4ZAKBZ1BQnXTmUw4bI6eAX
-# eU54BLpjITXFKdKT95iKb8UCqsKbPMYa8JA7mtxSKZGaM2ucRTn+sjCsTF/Jl+7q
-# P3PGYt3NAgMBAAGjdjB0MBMGA1UdJQQMMAoGCCsGAQUFBwMDMF0GA1UdAQRWMFSA
-# EFRLNfCSQeCkWjrGMLkdxfShLjAsMSowKAYDVQQDEyFQb3dlclNoZWxsIExvY2Fs
-# IENlcnRpZmljYXRlIFJvb3SCEDIKl+Pl1/+LSwfpbD8Ti50wCQYFKw4DAh0FAAOC
-# AQEAWathO3qne+05f5/fCGAMqfCbpr6praX+Z5S5Lqi9OfbKUVk5FUBvoKY1Nt61
-# bRKf6PLB+u1tdWlxauORZvMh6zaxv/GbyefWdFsAoi6Y7rrIjG1hYde9H24gR+3e
-# mZKjFnwJadygNbu5h42aPnhUkH+Mhw4OvU9IOKE7fqkDJo8ULHRIXA5CLDY5Zqh/
-# WIi6+u7Vg4ARMLeJ98EQn6eka1b6nKihQ56zD45QLbUwNLM8XvI1BxL5uJH448Vs
-# s10Z4uGYqZl5/+KDx82Sr3gQ9StlZ5JFw339VK3U7cmqnB2gZ4b/8trVuTY6F2jf
-# HsDM5YGK6LhuZJC2UjBdFqZOKjGCAeEwggHdAgEBMEAwLDEqMCgGA1UEAxMhUG93
-# ZXJTaGVsbCBMb2NhbCBDZXJ0aWZpY2F0ZSBSb290AhDQhKHJvulbh0eLWlfCmxcv
-# MAkGBSsOAwIaBQCgeDAYBgorBgEEAYI3AgEMMQowCKACgAChAoAAMBkGCSqGSIb3
-# DQEJAzEMBgorBgEEAYI3AgEEMBwGCisGAQQBgjcCAQsxDjAMBgorBgEEAYI3AgEV
-# MCMGCSqGSIb3DQEJBDEWBBQ8xuQJXNptQm2ihfHPTG8sjNahBzANBgkqhkiG9w0B
-# AQEFAASCAQBUZGCSvPZ2KMHGMVLJLfgXOY35m+NcZG3ngX699jSRx2/+ljT2igxV
-# XEEQM+vc7sJ/cOQ3pUoX+eUXxCk+PEMkveHf1M5zce2Se/ngVlbddMCFfyUJhLv2
-# bRdcKcpNgBT4mu2ieDv2ZXZvjczdsX5nK+qDKMQTHxYmH3jW6WMX43saa58YkDjl
-# KyduEUNoFweBlvh6NzSq13PIDCgpzB0Kfz/WZWHBAiMPGHb8FDDhWoPG91UlV9sn
-# C7skel32+vtTkcSskEiP79UOoem/wc7cTkhPIJd2/8cIg7dbdLiI7Jl8eROOPqdc
-# D4h4+tcL1zHGJIHpJjYSEUfcO7EUHWU2
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUF8MViKYT4TzS6lEQnfzxcz1l
+# P12gggL+MIIC+jCCAeKgAwIBAgIQPOk5qMHujKJIWIAsO7Qk9zANBgkqhkiG9w0B
+# AQsFADAVMRMwEQYDVQQDDApkb21pbmF0aW9uMB4XDTIzMDUyNDAwNTAyM1oXDTI0
+# MDUyNDAxMTAyM1owFTETMBEGA1UEAwwKZG9taW5hdGlvbjCCASIwDQYJKoZIhvcN
+# AQEBBQADggEPADCCAQoCggEBAMDYLgWqa3w5pMNPQ3gRVbpo+eOis8Ivljaz7Fm4
+# CIVTQNZX/2KKnqnkxFy2OpPrufDSHiK9mA7FQR1+6o/VJg2Jn55m/oMg/1RcFoT1
+# PYa72h+dispj10XyxI84bhAOsLtBp413nfzcFlaYUb1ZBDNMVqJjJEQAfc/GmSKq
+# RlmireDMReXc+c0hrvXfOyovoRcMsOoG7cPRVHxlQSMTynUWZsKiiEFoFwnfpc/k
+# zZJr15hAod64gJUy+h0vfusIsFGnOf9FCmFSX+2Xa36y0oEJNxiysXCFsPu2ZIiP
+# qKg7jfmFzJ8Mipsb33eS5Plmq9w9zkEWKpQwK8japeuml7kCAwEAAaNGMEQwDgYD
+# VR0PAQH/BAQDAgeAMBMGA1UdJQQMMAoGCCsGAQUFBwMDMB0GA1UdDgQWBBRqVyQh
+# 7zFy3sHlPYPeG96rKKxmkzANBgkqhkiG9w0BAQsFAAOCAQEAeLCMdveF82CsGRlD
+# xaygNW2ST4I1RJVZla5D3sw7sVMKhXiWM8AwS7+t+OIlaTkXA/zNP8t+M82WhtGP
+# 939bwpzzQUrzTs+d+3QOiAbsELmka/CHqnzH4gOZAApV0wXyxl3/Y/aP653U2RI/
+# mSHV6RLU+ShqRhB6p/sGXS+aN6dxwb+0Kpl7o511h49HniMOt+jSn1o51uIrV0u4
+# mN13wApW47oCiay5SOaqffupf29f9Rj5DwCeTvDzKepf5h+OTEEFrVvcstsGh7hK
+# FnCUSriQHjMSCzGfjG0EKJhx150q22YnuC7xD2mvWb/3Gf5TU0axf7AbDV5O46zT
+# 4Qr7djGCAcowggHGAgEBMCkwFTETMBEGA1UEAwwKZG9taW5hdGlvbgIQPOk5qMHu
+# jKJIWIAsO7Qk9zAJBgUrDgMCGgUAoHgwGAYKKwYBBAGCNwIBDDEKMAigAoAAoQKA
+# ADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGCNwIBBDAcBgorBgEEAYI3AgELMQ4wDAYK
+# KwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQxFgQU86DTTbDBOaibZxCKUu2kJkXqElIw
+# DQYJKoZIhvcNAQEBBQAEggEAku3EsfockpCHBc/4dxhmbNg2hFxTKklGG2herLgo
+# XoH1zSRJQNiZ9/qWvZDBENGtw74y5t72w50A8MKqLIf0QygRfdTAhqD0VFd48rHn
+# xqUEhjhUURrSNcNP0f0B12M35BDaBZpW7q3PhAUJnooN2NzBh4Je/BUkewcAndcY
+# oSCHDO8SnFvx5geypbkmp824XSctUf5ewVpfcZFw42nNjGbomIT4aGcarb1UxYlM
+# KlJ1RzNkUhbs8tpT3vOMdH4vppdyLz+J03aa8ntnb2aIWbTAl75MMHWM1iz5UH5g
+# 5GIUFDI5xS8bpcY9ir6zn6C5CRphTRjxz4d17Tpu5zMWAw==
 # SIG # End signature block
